98700ba61c2223e23be67b2b32dce95feaa308ec
Philip Dushie <philip.dushie@gmail.com>
2025-04-04 09:42:36 -0300

    creates view all listings page

--- app/(pages)/pets/page.jsx
+++ app/(pages)/pets/page.jsx
@@ -28,7 +28,7 @@ const PetList = () => {
 
   return (
     <div className="container mx-auto p-6">
-      <h2 className="text-3xl font-bold text-center mb-8 text-gray-800">Available Pets for Adoption</h2>
+      <h2 className="text-3xl font-bold text-center mb-8 text-gray-800">Pets Available for Adoption</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {pets.length > 0 ? (
           pets.map((pet) => (
