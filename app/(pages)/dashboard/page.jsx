7e323795cbc4362437484ac0dfc3c79fd5c4c111
Philip Dushie <philip.dushie@gmail.com>
2025-04-07 14:34:11 -0300

    fixes name display on dashboard

--- app/(pages)/dashboard/page.jsx
+++ app/(pages)/dashboard/page.jsx
@@ -3,15 +3,16 @@ import { useSession } from "next-auth/react";
 import Sidebar from "./_components/Sidebar";
 import Navbar from "../_components/Navbar";
 import Footer from "../_components/Footer";
+import { Loader } from "@/utils/loader";
 const Dashboard = () => {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
 
   return (
     <>
       <Navbar />
       <Sidebar />
       <div className="text-2xl flex justify-center items-center bg-white dark:text-white dark:bg-[#1D232A]">
-        Your are logged in as: {session?.user?.name ?? "Name not available"}
+        {status==="loading" ? <Loader /> : <p>Your are logged in as: {session?.user?.name}</p>}
       </div>
       <div className="min-h-[100vh] bg-white dark:bg-[#1D232A]"></div>
       <Footer />
